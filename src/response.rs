5504eb6caa2d35ea5e5ace5a4ce99f4f98ff3ef9
Justus Croskery <justus@olmmcc.tk>
2020-05-30 08:56:31 -0400

    Add log_game function

--- src/response.rs
+++ src/response.rs
@@ -1,4 +1,6 @@
 use std::collections::HashMap;
+use std::fs::OpenOptions;
+use std::io::Write;
 
 use serde_json::json;
 
@@ -6,8 +8,25 @@ fn message(message: &str) -> String {
     json!({ "message": message }).to_string()
 }
 
+async fn log_game(game_id: &str) {
+    let game_string = String::from("hi\n");
+    let mut file = OpenOptions::new().append(true).create(true).open("/var/log/olmmcc/games.log").unwrap();
+    file.write_all(game_string.as_bytes()).unwrap();
+}
+
 pub async fn formulate_response(url: &str, body: HashMap<&str, &str>) -> String {
     match url {
+        "/new_game" => new_game(body).await,
         _ => message(&format!("The provided url {} could not be resolved.", url))
     }
+}
+
+async fn new_game(body: HashMap<&str, &str>) -> String {
+    if body["type"] == "human" {
+
+    } else {
+
+    }
+    log_game("").await;
+    String::new()
 }
